fc3312898408698d30803930f49396b4ed380011
Taazim Temirova <tezi.temirova@gmail.com>
2020-04-30 13:08:27 +0600

    Lesson-65; created project and connected to the database

---
+++ model/src/test/java/edu/food/model/ModelApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.food.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
